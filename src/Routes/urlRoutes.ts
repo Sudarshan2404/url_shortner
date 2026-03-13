cd5a67d24db322921286aa55c2d6f447af9b3296
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2026-03-13 21:46:30 +0530

    imposed ratelimiter on all routes

--- src/Routes/urlRoutes.ts
+++ src/Routes/urlRoutes.ts
@@ -4,10 +4,11 @@ import {
   visiturl,
   shortenUrlcustom,
 } from "../controllers/urlcontrollers.js";
+import { ratelimiter } from "../middlewares/ratelimiter.js";
 
 const router = Router();
 
-router.post("/shortenurl", shortenUrl);
-router.get("/:code", visiturl);
-router.post("/cmshortenurl", shortenUrlcustom);
+router.post("/shortenurl", ratelimiter, shortenUrl);
+router.get("/:code", ratelimiter, visiturl);
+router.post("/cmshortenurl", ratelimiter, shortenUrlcustom);
 export default router;
